b201c5f287245b91468bd4c137edb041c8dea3c7
golabi159 <golabi159@yahoo.com>
2025-03-03 19:15:46 +0330

    Update fluxdev.sh

--- fluxdev.sh
+++ fluxdev.sh
@@ -3,7 +3,7 @@
 source /venv/main/bin/activate
 COMFYUI_DIR=${WORKSPACE}/ComfyUI
 
-HF_TOKEN="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"
+# Packages are installed after nodes so we can fix them...
 
 APT_PACKAGES=(
     #"package-1"
@@ -22,75 +22,72 @@ NODES=(
      "https://github.com/rgthree/rgthree-comfy.git"
      "https://github.com/Fannovel16/comfyui_controlnet_aux.git"
      "https://github.com/kaibioinfo/ComfyUI_AdvancedRefluxControl.git"
+
 )
+
 WORKFLOWS=(
     "https://gist.githubusercontent.com/robballantyne/f8cb692bdcd89c96c0bd1ec0c969d905/raw/2d969f732d7873f0e1ee23b2625b50f201c722a5/flux_dev_example.json"
 )
 
-CLIP_MODELS=(
-    "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors"
-    "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors"
-    "https://huggingface.co/zer0int/CLIP-GmP-ViT-L-14/resolve/main/ViT-L-14-TEXT-detail-improved-hiT-GmP-TE-only-HF.safetensors"
+CHECKPOINT_MODELS=(
+    "https://civitai.com/api/download/models/798204?type=Model&format=SafeTensor&size=full&fp=fp16"
 )
 
-CLIPV_MODELS=(
-    "https://huggingface.co/Comfy-Org/sigclip_vision_384/resolve/main/sigclip_vision_patch14_384.safetensors"
+UNET_MODELS=(
 )
 
-LORAS_MODELS=(
-       "https://huggingface.co/prithivMLmods/Ton618-Epic-Realism-Flux-LoRA/resolve/main/Epic-Realism-Unpruned.safetensors"
-       "https://huggingface.co/strangerzonehf/Flux-Super-Realism-LoRA/resolve/main/super-realism.safetensors"
-       "https://huggingface.co/prithivMLmods/Flux-Dalle-Mix-LoRA/resolve/main/dalle-mix.safetensors"
-       "https://huggingface.co/strangerzonehf/Flux-Midjourney-Mix-LoRA/resolve/main/midjourney-mix.safetensors"
-       "https://huggingface.co/strangerzonehf/Flux-Super-Blend-LoRA/resolve/main/Super-Blend.safetensors"
-       "https://huggingface.co/prithivMLmods/Flux-Fine-Detail-LoRA/resolve/main/Fine-Detail.safetensors"
-       "https://huggingface.co/XLabs-AI/flux-lora-collection/resolve/main/mjv6_lora.safetensors"
-       "https://huggingface.co/XLabs-AI/flux-RealismLora/resolve/main/lora.safetensors"
-       "https://huggingface.co/strangerzonehf/Flux-Super-Realism-LoRA/resolve/main/super-realism.safetensors"
-       "https://huggingface.co/XLabs-AI/flux-lora-collection/resolve/main/anime_lora.safetensors"
-       "https://huggingface.co/XLabs-AI/flux-lora-collection/resolve/main/art_lora.safetensors"
-       "https://huggingface.co/XLabs-AI/flux-lora-collection/resolve/main/disney_lora.safetensors"
-       "https://huggingface.co/XLabs-AI/flux-lora-collection/resolve/main/furry_lora.safetensors"
-       "https://huggingface.co/XLabs-AI/flux-lora-collection/resolve/main/realism_lora.safetensors"
-       "https://huggingface.co/XLabs-AI/flux-lora-collection/resolve/main/scenery_lora.safetensors"
+LORA_MODELS=(
 )
 
-CTRL_MODELS=(
-       "https://huggingface.co/XLabs-AI/flux-controlnet-canny-v3/resolve/main/flux-canny-controlnet-v3.safetensors"
-       "https://huggingface.co/XLabs-AI/flux-controlnet-collections/resolve/main/flux-depth-controlnet-v3.safetensors"
-       "https://huggingface.co/XLabs-AI/flux-controlnet-collections/resolve/main/flux-hed-controlnet-v3.safetensors"
+VAE_MODELS=(
 )
 
+ESRGAN_MODELS=(
+)
 
-UNET_MODELS=()
-VAE_MODELS=()
+CONTROLNET_MODELS=(
+)
+
+### DO NOT EDIT BELOW HERE UNLESS YOU KNOW WHAT YOU ARE DOING ###
 
 function provisioning_start() {
     provisioning_print_header
     provisioning_get_apt_packages
     provisioning_get_nodes
     provisioning_get_pip_packages
-    workflows_dir="${COMFYUI_DIR}/user/default/workflows"
-    mkdir -p "${workflows_dir}"
-    provisioning_get_files "${workflows_dir}" "${WORKFLOWS[@]}"
-    provisioning_download "https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/flux1-dev.safetensors" "${COMFYUI_DIR}/models/unet"
-    provisioning_download "https://huggingface.co/black-forest-labs/FLUX.1-Fill-dev/resolve/main/flux1-fill-dev.safetensors" "${COMFYUI_DIR}/models/unet"
-    provisioning_download "https://huggingface.co/black-forest-labs/FLUX.1-Redux-dev/resolve/main/flux1-redux-dev.safetensors" "${COMFYUI_DIR}/models/style_models"
-    wget --header="Authorization: Bearer $HF_TOKEN" "https://huggingface.co/alimama-creative/FLUX.1-Turbo-Alpha/resolve/main/diffusion_pytorch_model.safetensors" -O ${COMFYUI_DIR}/models/loras/FLUX.1-Turbo-Alpha.safetensors
-    provisioning_download "https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/ae.safetensors" "${COMFYUI_DIR}/models/vae"
-    provisioning_get_files "${COMFYUI_DIR}/models/clip" "${CLIP_MODELS[@]}"
-    provisioning_get_files "${COMFYUI_DIR}/models/clip_vision" "${CLIPV_MODELS[@]}"
-    provisioning_get_files "${COMFYUI_DIR}/models/loras" "${LORAS_MODELS[@]}"
-    provisioning_get_files "${COMFYUI_DIR}/models/xlabs/controlnets" "${CTRL_MODELS[@]}"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/checkpoints" \
+        "${CHECKPOINT_MODELS[@]}"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/unet" \
+        "${UNET_MODELS[@]}"
+wget --header="Authorization: Bearer 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" "https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/flux1-dev.safetensors" -O ${COMFYUI_DIR}/models/unet/flux1-dev.safetensors 
+
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/lora" \
+        "${LORA_MODELS[@]}"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/controlnet" \
+        "${CONTROLNET_MODELS[@]}"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/vae" \
+        "${VAE_MODELS[@]}"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/esrgan" \
+        "${ESRGAN_MODELS[@]}"
     provisioning_print_end
 }
 
 function provisioning_get_apt_packages() {
-    [[ -n $APT_PACKAGES ]] && sudo $APT_INSTALL ${APT_PACKAGES[@]}
+    if [[ -n $APT_PACKAGES ]]; then
+            sudo $APT_INSTALL ${APT_PACKAGES[@]}
+    fi
 }
 
 function provisioning_get_pip_packages() {
-    [[ -n $PIP_PACKAGES ]] && pip install --no-cache-dir ${PIP_PACKAGES[@]}
+    if [[ -n $PIP_PACKAGES ]]; then
+            pip install --no-cache-dir ${PIP_PACKAGES[@]}
+    fi
 }
 
 function provisioning_get_nodes() {
@@ -99,35 +96,94 @@ function provisioning_get_nodes() {
         path="${COMFYUI_DIR}custom_nodes/${dir}"
         requirements="${path}/requirements.txt"
         if [[ -d $path ]]; then
-            [[ ${AUTO_UPDATE,,} != "false" ]] && ( cd "$path" && git pull && [[ -e $requirements ]] && pip install --no-cache-dir -r "$requirements" )
+            if [[ ${AUTO_UPDATE,,} != "false" ]]; then
+                printf "Updating node: %s...\n" "${repo}"
+                ( cd "$path" && git pull )
+                if [[ -e $requirements ]]; then
+                   pip install --no-cache-dir -r "$requirements"
+                fi
+            fi
         else
-            git clone "$repo" "$path" --recursive && [[ -e $requirements ]] && pip install --no-cache-dir -r "$requirements"
+            printf "Downloading node: %s...\n" "${repo}"
+            git clone "${repo}" "${path}" --recursive
+            if [[ -e $requirements ]]; then
+                pip install --no-cache-dir -r "${requirements}"
+            fi
         fi
     done
 }
 
 function provisioning_get_files() {
-    [[ -z $2 ]] && return 1
+    if [[ -z $2 ]]; then return 1; fi
+    
     dir="$1"
     mkdir -p "$dir"
     shift
-    for url in "$@"; do
-        provisioning_download "$url" "$dir"
+    arr=("$@")
+    printf "Downloading %s model(s) to %s...\n" "${#arr[@]}" "$dir"
+    for url in "${arr[@]}"; do
+        printf "Downloading: %s\n" "${url}"
+        provisioning_download "${url}" "${dir}"
+        printf "\n"
     done
 }
 
 function provisioning_print_header() {
-    printf "\n##############################################\n# Provisioning container...                  #\n# This will take some time...                #\n# Your container will be ready on completion #\n##############################################\n\n"
+    printf "\n##############################################\n#                                            #\n#          Provisioning container            #\n#                                            #\n#         This will take some time           #\n#                                            #\n# Your container will be ready on completion #\n#                                            #\n##############################################\n\n"
 }
 
 function provisioning_print_end() {
-    printf "\nProvisioning complete: Application will start now\n\n"
+    printf "\nProvisioning complete:  Application will start now\n\n"
 }
 
+function provisioning_has_valid_hf_token() {
+    [[ -n "$HF_TOKEN" ]] || return 1
+    url="https://huggingface.co/api/whoami-v2"
+
+    response=$(curl -o /dev/null -s -w "%{http_code}" -X GET "$url" \
+        -H "Authorization: Bearer $HF_TOKEN" \
+        -H "Content-Type: application/json")
+
+    # Check if the token is valid
+    if [ "$response" -eq 200 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+function provisioning_has_valid_civitai_token() {
+    [[ -n "$CIVITAI_TOKEN" ]] || return 1
+    url="https://civitai.com/api/v1/models?hidden=1&limit=1"
+
+    response=$(curl -o /dev/null -s -w "%{http_code}" -X GET "$url" \
+        -H "Authorization: Bearer $CIVITAI_TOKEN" \
+        -H "Content-Type: application/json")
+
+    # Check if the token is valid
+    if [ "$response" -eq 200 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# Download from $1 URL to $2 file path
 function provisioning_download() {
-    local auth_header=""
-    [[ $1 =~ ^https://([a-zA-Z0-9_-]+\.)?huggingface\.co(/|$|\?) ]] && auth_header="Authorization: Bearer $HF_TOKEN"
-    wget --header="$auth_header" -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
+    if [[ -n $HF_TOKEN && $1 =~ ^https://([a-zA-Z0-9_-]+\.)?huggingface\.co(/|$|\?) ]]; then
+        auth_token="$HF_TOKEN"
+    elif 
+        [[ -n $CIVITAI_TOKEN && $1 =~ ^https://([a-zA-Z0-9_-]+\.)?civitai\.com(/|$|\?) ]]; then
+        auth_token="$CIVITAI_TOKEN"
+    fi
+    if [[ -n $auth_token ]];then
+        wget --header="Authorization: Bearer $auth_token" -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
+    else
+        wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
+    fi
 }
 
-[[ ! -f /.noprovisioning ]] && provisioning_start
+# Allow user to disable provisioning if they started with a script they didn't want
+if [[ ! -f /.noprovisioning ]]; then
+    provisioning_start
+fi
